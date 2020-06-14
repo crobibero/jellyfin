606000f6ea2430b23a8d546eb5eb8303c0b6a55f
crobibero <cody@robibe.ro>
2020-06-14 14:32:24 -0600

    add basic user unit tests

---
+++ tests/Jellyfin.Server.Implementations.Tests/Users/UserTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using MediaBrowser.Controller.Library;
+using Tests.Common;
+using Xunit;
+
+namespace Jellyfin.Server.Implementations.Tests.Users
+{
+    public class UserTests : IClassFixture<JellyfinApplicationFactory>
+    {
+        private readonly IUserManager _userManager;
+
+        public UserTests(JellyfinApplicationFactory factory)
+        {
+            _userManager = (IUserManager)factory.Services.GetService(typeof(IUserManager));
+        }
+
+        [Fact]
+        public void CreateUser()
+        {
+            var user = _userManager.CreateUser("jellyfin");
+            Assert.True(_userManager.Users.Count() == 1);
+            Assert.True(_userManager.Users.First().Id == user.Id);
+        }
+    }
+}
