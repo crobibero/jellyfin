b04f62d97de52649980a8240686748452890fbf9
Cody Robibero <cody@robibe.ro>
2022-04-01 08:21:00 -0600

    Add IEnumerable index extensions

--- src/Jellyfin.Extensions/EnumerableExtensions.cs
+++ src/Jellyfin.Extensions/EnumerableExtensions.cs
@@ -47,5 +47,41 @@ namespace Jellyfin.Extensions
 
             return false;
         }
+
+        /// <summary>
+        /// Finds the index of the first item matching an expression in an enumerable.
+        /// </summary>
+        /// <param name="items">The enumerable to search.</param>
+        /// <param name="predicate">The expression to test the items against.</param>
+        /// <typeparam name="T">The type if item.</typeparam>
+        /// <returns>The index of the first matching item, or -1 if no items match.</returns>
+        public static int FindIndex<T>(this IEnumerable<T> items, Func<T, bool> predicate)
+        {
+            ArgumentNullException.ThrowIfNull(items);
+            ArgumentNullException.ThrowIfNull(predicate);
+
+            var index = 0;
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    return index;
+                }
+
+                index++;
+            }
+
+            return -1;
+        }
+
+        /// <summary>Finds the index of the first occurrence of an item in an enumerable.</summary>
+        /// <param name="items">The enumerable to search.</param>
+        /// <param name="item">The item to find.</param>
+        /// <typeparam name="T">The type if item.</typeparam>
+        /// <returns>The index of the first matching item, or -1 if the item was not found.</returns>
+        public static int IndexOf<T>(this IEnumerable<T> items, T item)
+        {
+            return items.FindIndex(i => EqualityComparer<T>.Default.Equals(item, i));
+        }
     }
 }
